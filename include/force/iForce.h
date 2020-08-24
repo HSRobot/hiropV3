1e69c47a7a2fe4d6ef7c4e710b7db070141db76f
WangNeng <1252773119@qq.com>
2020-08-24 10:59:47 +0800

    添加阻抗插件

---
+++ include/force/iForce.h
@@ -0,0 +1,88 @@
+#ifndef IGRIPPER_H
+#define IGRIPPER_H
+
+#pragma once
+
+#include <yaml-cpp/yaml.h>
+#include "hplugin.h"
+#include <vector>
+#include <force/forceExport.h>
+#include <string>
+namespace hirop_force {
+
+class IForce{
+public:
+
+        /**
+         * @brief 解析私有参数
+         * @return
+         */
+        virtual int parseConfig(YAML::Node& ) = 0;
+
+        /**
+         * @brief setInputForce 输入力矩参数
+         * @return
+         */
+        virtual void setInputForceBias(std::vector<double> &value) = 0;
+
+        /***
+         *
+         * @param direction 设置阻抗算法方向使能参数
+         * @return
+         */
+
+        virtual int setDirection(int *direction) = 0;
+
+        /**
+         *
+         * @param _Stiffness 刚度
+         * @param _Damping  阻尼
+         * @param _Mass   质量
+         * @return
+         */
+        virtual int setParameters(double* _Stiffness,double* _Damping,double* _Mass)=0;
+
+
+        /**
+         * @brief setInputRobotPose 输入机器人的笛卡尔坐标
+         * @return
+         */
+        virtual void setInputRobotCartPose(std::vector<double> &value) = 0;
+
+        /**
+         * @brief compute 算法运算
+         * @return
+         */
+        virtual int compute() = 0;
+
+        /**
+         * @brief getResult 获取力控运算的输出结果
+         * @return
+         */
+        virtual int getResult(std::vector<double> &) = 0;
+
+
+        /**
+         * @brief getName  获取算法插件的名称
+         * @param name
+         * @return
+         */
+        virtual std::string  getName() = 0;
+
+
+        /**
+         * @brief printInfo 输出打印信息
+         * @return
+         */
+        virtual int printInfo() = 0;
+protected:
+    // 保存器的字符描述
+    std::string name;
+
+    // 其算法实现类别 C++ / Python
+    hirop_force::ENTITY_TYPE entityType;
+};
+
+}
+H_DECLARE_INTERFACE(hirop_force::IForce,"IForceV1.0")
+#endif
